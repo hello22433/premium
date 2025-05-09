c399b8cda0f527311226dfad2391d1dc378093ac
nhsKevin <jjkevin.nhs@gmail.com>
2025-05-09 13:48:47 +0900

    feat : param kind 수정

--- src/partner_company_extern/infra/galaxia.http.ts
+++ src/partner_company_extern/infra/galaxia.http.ts
@@ -146,7 +146,7 @@ export class GalaxiaHttp implements IGalaxia {
     });
 
     try {
-      const sendUrl = `${url}?${data.toString()}&issueDay=${obj.sendRequestAt}&=paramKind1`;
+      const sendUrl = `${url}?${data.toString()}&issueDay=${obj.sendRequestAt}&paramKind=1`;
       this.logger.log(sendUrl);
       this.logger.log(headers);
 
