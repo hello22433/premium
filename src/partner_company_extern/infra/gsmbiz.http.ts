400f9c912a56f84143fb0113a887a9a31530d630
jun <development@enmad.com>
2025-05-12 15:52:03 +0900

    gsm port 수정

--- src/partner_company_extern/infra/gsmbiz.http.ts
+++ src/partner_company_extern/infra/gsmbiz.http.ts
@@ -24,7 +24,7 @@ export class GsmbizHttp implements IGsmbiz {
     this.encKey = this.configService.getOrThrow('GS_M_BIZ_ENCKEY');
     this.encIv = this.configService.getOrThrow('GS_M_BIZ_ENCIV');
     if (this.configService.getOrThrow('ENVIRONMENT') === 'prod') {
-      this.url = 'https://api.gsmcoupon.gsmpp.com'; // TODO prod URL 받으면 변경
+      this.url = 'https://api.gsmcoupon.gsmpp.com:32443'; // TODO prod URL 받으면 변경
     }
   }
 
