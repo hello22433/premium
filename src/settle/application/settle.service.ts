24692cd4cf30a9a5343a0b43c39fe5d7eab4c339
Caelum-Windows <development@enmad.com>
2025-11-13 10:11:04 +0900

    :bulb: Fix: 잔여한도 조회 API 관련
    - 초기화되어있던거 다시 수정

--- src/settle/application/settle.service.ts
+++ src/settle/application/settle.service.ts
@@ -1547,8 +1547,8 @@ export class SettleService {
     // 발송금액 = 서비스금액 + 정산기일초과금액
     const deliveryAmount = userEntity.serviceAmount + overdueAmount;
 
-    // 잔여발송한도 = 최대서비스한도 + 선입금금액 - 발송금액 + 정산금액
-    const remainServiceAmount = userEntity.maximumLimit + userEntity.balance - deliveryAmount + userEntity.allSettleAmount;
+    // 잔여발송한도 = 최대서비스한도 - 주문금액
+    const remainServiceAmount = userEntity.maximumLimit - userEntity.allSettleAmount;
 
     return {
       maximumLimit: userEntity.maximumLimit,
