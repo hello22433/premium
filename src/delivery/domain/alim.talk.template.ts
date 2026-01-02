a1b8e565027f1244f59e01f8c81d46ea9717f23e
Caelum-Windows <development@enmad.com>
2026-01-02 17:41:54 +0900

    :zap: Perf: 이벤트 이름 대신 발신제목 사용

--- src/delivery/domain/alim.talk.template.ts
+++ src/delivery/domain/alim.talk.template.ts
@@ -37,7 +37,7 @@ export const AlimTalkTemplate = (orderDelivery: OrderDeliveryEntity) => {
 
 이 메시지는 알림톡 테스트 메시지 입니다.
 
-${orderDelivery.orderProductMapping.order.eventName} 당첨을 축하드립니다.
+${orderDelivery.orderProductMapping.sendTitle ?? ''} 당첨을 축하드립니다.
 문의사항은 고객센터 번호를 통해 문의하시길 바랍니다.
 이 메시지는 고객님의 동의에 의해 지급된 쿠폰 안내 메시지입니다.`;
   }
@@ -50,7 +50,7 @@ ${orderDelivery.orderProductMapping.order.eventName} 당첨을 축하드립니�
 고객센터 : 1644-3614
 발행자 : ${orderDelivery.orderProductMapping.order.user!.company?.businessName ?? ''}
 
-${orderDelivery.orderProductMapping.order.eventName} 당첨을 축하드립니다.
+${orderDelivery.orderProductMapping.sendTitle ?? ''} 당첨을 축하드립니다.
 문의사항은 고객센터 번호를 통해 문의하시길 바랍니다.
 이 메시지는 고객님의 동의에 의해 지급된 쿠폰 안내 메시지입니다.`;
 };
