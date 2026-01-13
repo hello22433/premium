6f3293f8ccfd61be6414ff5bd874dda36a47d08e
Caelum-Windows <development@enmad.com>
2026-01-13 13:25:25 +0900

    feat: auth-list API에 allowedSendMethods 추가
    - 프론트엔드에서 주문 등록/수정 시 허용 발신수단 조회 가능

--- src/user_info/api/user.info.res.dto.ts
+++ src/user_info/api/user.info.res.dto.ts
@@ -1,5 +1,6 @@
 import { UserAuthMainEnum, UserAuthSubEnum } from '../../user_management/domain/user.auth.enum';
 import { ApiProperty } from '@nestjs/swagger';
+import { IOrderSendMethod } from '../../order/interface/order.send.method';
 
 export class UserGetAuthListResDto {
   @ApiProperty({
@@ -11,4 +12,12 @@ export class UserGetAuthListResDto {
     description: '서브 메뉴 리스트',
   })
   subMenuList: UserAuthSubEnum[];
+
+  @ApiProperty({
+    description: '허용 발신수단 목록',
+    example: ['ALIM_TALK', 'SMS', 'EMAIL'],
+    enum: IOrderSendMethod,
+    isArray: true,
+  })
+  allowedSendMethods: IOrderSendMethod[];
 }
