8eb84f030bc2b70e8b9bd23c90f08ecb7e6935d7
Caelum-Windows <development@enmad.com>
2026-01-19 17:39:40 +0900

    fix: 독려문자 형식 변경

--- src/delivery/domain/sms.encourage.template.ts
+++ src/delivery/domain/sms.encourage.template.ts
@@ -2,9 +2,9 @@ import { format } from 'date-fns';
 import { OrderDeliveryEntity } from '../../entity/order.delivery.entity';
 
 export const smsEncourageTemplate = (orderDelivery: OrderDeliveryEntity) => {
-  return `미사용 쿠폰에 대한 유효기간 안내
+  return `쿠폰 기간만료안내
 
-쿠폰 만료일: ${format(orderDelivery.expireAt!, 'yyyy-MM-dd')}
+만료일: ${format(orderDelivery.expireAt!, 'yyyy-MM-dd')}
 
-※기간 만료시 재발송, 기간연장, 환불 등 처리불가`;
+※기간만료시 재발송/기간연장/환불 불가`;
 };
