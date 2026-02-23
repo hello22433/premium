4f9b196b468e95575cbde5fa79507832d92fda0d
Caelum-Windows <development@enmad.com>
2026-02-23 14:15:30 +0900

    feat: 비SSG 상품 MMS 발송 시 쿠폰 정보 텍스트 추가
    - smsCouponInfoTemplate 생성 (상품명, 유효기간, 쿠폰번호)
    - buildSmsText()에서 비SSG, 비초이스 상품에 쿠폰 정보 추가

---
+++ src/delivery/domain/sms.coupon.info.template.ts
@@ -0,0 +1,12 @@
+import { format } from 'date-fns';
+import { OrderDeliveryEntity } from '../../entity/order.delivery.entity';
+
+export const smsCouponInfoTemplate = (orderDelivery: OrderDeliveryEntity): string => {
+  const product = orderDelivery.orderProductMapping.product;
+  const expireAt = orderDelivery.expireAt ? format(orderDelivery.expireAt, 'yyyy-MM-dd') : '';
+
+  return `
+
+▷상품명: ${product.name}${expireAt ? `\n▷유효기간: ${expireAt} 까지` : ''}
+▷쿠폰번호: ${orderDelivery.barCode}`;
+};
