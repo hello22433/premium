a99bded8d705e41317856caa71ea70ad6ebe2441
Caelum-Windows <development@enmad.com>
2026-03-11 14:32:57 +0900

    로그인 인증코드 템플릿 수정

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -381,7 +381,7 @@ export class UserService {
     const code = generateNumericCode(5);
     const expireAt = addMinutes(new Date(), EmailCertifyExpireMinute);
 
-    const messageText = `이팝콘 프리미엄 로그인 인증코드\n[#${code}]`;
+    const messageText = `이팝콘 프리미엄 로그인 인증코드\n[${code}]`;
 
     // 알림톡 발송 시도 → 실패 시 SMS 대체 발송
     try {
