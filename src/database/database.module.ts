b8280300e11aec64e6c522f28e31435496ec4d6f
Caelum-Windows <development@enmad.com>
2026-06-15 17:43:06 +0900

    fix: SsgResendDeductRecoveryLogEntity DataSource entities 배열 등록 누락 수정
    forFeature 에만 등록되고 글로벌 entities[] 누락 → autoLoadEntities 미사용이라
    부팅 시 repository 주입 실패(Entity metadata not found) 크래시

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -28,6 +28,7 @@ import { EmailSendHistoryEntity } from '../entity/email.send.history.entity';
 import { SqlLogger } from '../common/api/sql.logger';
 import { SsgEventAmountHistoryEntity } from '../entity/ssg.event.amount.history.entity';
 import { SsgEventRecoveryLogEntity } from '../entity/ssg.event.recovery.log.entity';
+import { SsgResendDeductRecoveryLogEntity } from '../entity/ssg.resend.deduct.recovery.log.entity';
 import { SsgEventEntity } from '../entity/ssg.event.entity';
 import { SsgReservationRangeEntity } from '../entity/ssg.reservation.range.entity';
 import { GemteckMsgQueueEntity } from '../entity/gemtek/msg.queue.entity';
@@ -126,6 +127,7 @@ import { ForbiddenWordBlockLogEntity } from '../entity/forbidden.word.block.log.
           EmailSendHistoryEntity,
           SsgEventAmountHistoryEntity,
           SsgEventRecoveryLogEntity,
+          SsgResendDeductRecoveryLogEntity,
           SsgEventEntity,
           SsgReservationRangeEntity,
           QnaEntity,
